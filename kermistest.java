d2291c1a7970bd7d8621ceb77ad72d5fb034feaf
AnilJava1 <amahabali@hotmail.com>
2018-07-18 11:54:40 +0200

    2e commit, foutmelding scanner

--- kermistest.java
+++ kermistest.java
@@ -6,12 +6,18 @@ public class kermistest {
 
 	public static void main(String[] args) {
 				
-				System.out.println("Kies een attractie die je wilt proberen.\n 1 voor Botsauto's \n");
-				Scanner scanner = new Scanner(System.in);
-				String antwoord = scanner.nextLine();
-				System.out.println(antwoord + "Dat is fout.");
+				System.out.println("Kies een attractie die je wilt proberen.\n1 voor Botsauto's\n2 voor de Spin\n3 voor het Spiegelpaleis\n4 voor het Spookhuis\n5 voor Hawaii en\n6 voor Ladderklimmen");
+				Scanner sc = new Scanner(System.in);
+				String antwoord = sc.nextLine();
+				
+				System.out.println(antwoord + "..........."); // op de puntjes wil je later de keuze zien die gelinkt is aan de keuze: 1 voor botsauto's bijv.
+				if (antwoord >= 7 < 0){							// ik wil hier graag ingeven dat de input in de scanner alleen 0 t/m 7 mag zijn. Indien een 8 wordt gekozen wil ik graag dat de syso op lijn 15 print.
+					System.out.println("Je kan alleen een keuze van 0 t/m 6 ingeven");
 				}
+	
 	}
+	
+}
 
 
 
@@ -26,7 +32,9 @@ class Attractie{
  *Stap 2, funct.doelst.: Creëer subclass: Botsauto's. Gebruik hiervoor extend omdat botsauto een onderdeel is van attractie. Check.
  *Stap 3, funct.doelst.: Creëer de overige subclassen: Spin, Spiegelpaleis, Spookhuis, Hawaii, Ladderklimmen. Check.
  Controle: wat heb je nu: hoofdklasse = Attractie. Subclassen zijn de 'activiteiten'.  Zijn deze 'verbonden' met de super Attractie? Ja want alles staat op public
- *Stap 4, funct.doelst.: maak een scanner waarin input gegeven kan worden.
+ *Stap 4, funct.doelst.: maak een scanner waarin input gegeven kan worden. Check.
+ *Stap 5,: funct.doelst.: zorg dat er alleen de keuze's 1 t/m 6 gegeven kan worden. 
+ *Stap 5, funct.doelst.: link de scanner aan de attractie.
 
 
 
