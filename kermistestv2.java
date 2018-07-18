4b4991d7a053caaa43152226b6c608be1c9a6c48
AnilJava1 <amahabali@hotmail.com>
2018-07-18 13:49:36 +0200

    kermistestv2

--- kermistestv2.java
+++ kermistestv2.java
@@ -10,11 +10,20 @@ public class kermistestv2 {
 
 
 class Attractie{
+	String naam;
+	int attracties;
+	String kaart;
+	void kaartbestellen () {
+								// hier wil ik dan een methode maken waarmee iemand een kaart kan bestellen maar nu heb ik geen idee hoe dat in zijn werk gaat.
 	}
+	
+}
 
 
-/*Nieuwe poging. Niet met een scanner werken want dat lukt me toch niet.
- * Stap 1: de diverse klassen heb je al maar niet de class Attractie.
+/*Nieuwe poging. Niet met een scanner werken aangezien dat toch niet lukt.
+ * Stap 1: de diverse klassen heb je al maar niet de class Attractie. Maak class Attractie. Check.
+ * Stap 2: even je gedachten op iets anders, werk de kenmerken van de klassen en subclassen uit.
+ * 
  * 
  * 
  * 
